8733a8049036015abdadc83ff79c1b2a07b6691a
Andrew <andrew.1.kulikov@gmail.com>
2022-01-02 12:45:06 +0300

    Add event detector tests

--- src/AtmReportGenerator.Tests/EventDetectorTests.cs
+++ src/AtmReportGenerator.Tests/EventDetectorTests.cs
@@ -1,5 +1,4 @@
-﻿using System.Linq;
-using AtmReportGenerator.Entities;
+﻿using AtmReportGenerator.Entities;
 using AtmReportGenerator.Processors;
 using Xunit;
 
@@ -10,32 +9,61 @@ namespace AtmReportGenerator.Tests
         [Fact]
         public void UnloadEvent_ShouldNotBeDetected_WhenRemainingAmountBecameLessZero()
         {
-            var series = TestSeriesGenerator.FromMovements(new[]
-            {
+            var series = TestSeriesGenerator.FromMovements(
                 (0d, 0d),
                 (-590, 590),
                 (-1450, 860),
-                (52885, 1770)
-            }).ToList();
+                (52885, 1770));
 
             var unloadEventDetector = new CashUnloadEventDetector();
 
-            for (int i = 0; i < series.Count - 1; i++)
-            {
-                Assert.False(unloadEventDetector.Match(series, i));
-            }
+            for (var i = 0; i < series.Count - 1; i++) Assert.False(unloadEventDetector.Match(series, i));
+        }
+
+        [Fact]
+        public void UnloadEvent_ShouldDetectEvent_WhenNextIsZero()
+        {
+            var series = TestSeriesGenerator.FromMovements(
+                (10000d, 100d),
+                (0, 0),
+                (70000d, 860));
+
+            var unloadEventDetector = new CashUnloadEventDetector();
+
+            Assert.True(unloadEventDetector.Match(series, 0));
+            Assert.False(unloadEventDetector.Match(series, 1));
+
+            var result = unloadEventDetector.Produce(series, 0) as AtmCashUnloadEvent;
+
+            Assert.NotNull(result);
+            Assert.Equal(10000d, result.Amount);
+        }
+
+        [Fact]
+        public void UnloadEvent_ShouldDetectEvent_WhenAmountIncreasedImmediately()
+        {
+            var series = TestSeriesGenerator.FromMovements(
+                (10000d, 100d),
+                (70000d, 860));
+
+            var unloadEventDetector = new CashUnloadEventDetector();
+
+            Assert.True(unloadEventDetector.Match(series, 0));
+
+            var result = unloadEventDetector.Produce(series, 0) as AtmCashUnloadEvent;
+
+            Assert.NotNull(result);
+            Assert.Equal(10000d, result.Amount);
         }
 
         [Fact]
         public void LoadEvent_ShouldProcessCorrectly_WhenRemainingAmountBecameLessZero()
         {
-            var series = TestSeriesGenerator.FromMovements(new[]
-            {
+            var series = TestSeriesGenerator.FromMovements(
                 (0d, 0d),
                 (-590, 590),
                 (-1450, 860),
-                (52885, 1770)
-            }).ToList();
+                (52885, 1770));
 
             var loadEventDetector = new CashLoadEventDetector();
 
@@ -44,9 +72,45 @@ namespace AtmReportGenerator.Tests
             Assert.True(loadEventDetector.Match(series, 2));
 
             var result = loadEventDetector.Produce(series, 2) as AtmCashLoadEvent;
-            
+
             Assert.NotNull(result);
             Assert.Equal(52885 + 1770 + 860 + 590, result.Amount);
+        }
+
+        [Fact]
+        public void LoadEvent_ShouldDetectEvent_WhenCurrentIsZero()
+        {
+            var series = TestSeriesGenerator.FromMovements(
+                (10000d, 100d),
+                (0, 0),
+                (70000d, 860));
+
+            var loadEventDetector = new CashLoadEventDetector();
+
+            Assert.False(loadEventDetector.Match(series, 0));
+            Assert.True(loadEventDetector.Match(series, 1));
+
+            var result = loadEventDetector.Produce(series, 1) as AtmCashLoadEvent;
+
+            Assert.NotNull(result);
+            Assert.Equal(70860d, result.Amount);
+        }
+
+        [Fact]
+        public void LoadEvent_ShouldDetectEvent_WhenAmountIncreasedImmediately()
+        {
+            var series = TestSeriesGenerator.FromMovements(
+                (10000d, 100d),
+                (70000d, 860));
+
+            var loadEventDetector = new CashLoadEventDetector();
+
+            Assert.True(loadEventDetector.Match(series, 0));
+
+            var result = loadEventDetector.Produce(series, 0) as AtmCashLoadEvent;
+
+            Assert.NotNull(result);
+            Assert.Equal(70860d, result.Amount);
         }
     }
 }
--- src/AtmReportGenerator.Tests/TestSeriesGenerator.cs
+++ src/AtmReportGenerator.Tests/TestSeriesGenerator.cs
@@ -7,15 +7,18 @@ namespace AtmReportGenerator.Tests
 {
     public static class TestSeriesGenerator
     {
-        public static AtmLog BuildLogFromMovements((double, double)[] logs) =>
+        public static AtmLog BuildLogFromMovements(params (double, double)[] logs) =>
             new AtmLog
             {
                 AtmId = "Test1234",
                 AtmInfo = "Test1234 Минск",
-                Logs = FromMovements(logs).ToList()
+                Logs = FromMovements(logs)
             };
 
-        public static IEnumerable<AtmLogRecord> FromMovements((double, double)[] logs)
+        public static List<AtmLogRecord> FromMovements(params (double, double)[] logs) => 
+            EnumerateMovements(logs).ToList();
+
+        private static IEnumerable<AtmLogRecord> EnumerateMovements(params (double, double)[] logs)
         {
             var currentTime = new DateTime(2021, 11, 1, 7, 0, 0);
             var measureStep = TimeSpan.FromMinutes(15);
